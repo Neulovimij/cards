c7a54407a112dd3d5da0f88428dd92a9bcdbe5cf
Alex Samal <70150329+Neulovimij@users.noreply.github.com>
2021-08-19 18:25:06 +0300

    unit commit

---
+++ src/f1-main/m1-ui/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from '../../logo.svg';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+{/* # hr, prov*/}
+      <>
+        {/*<Header/>*/}
+      </>
+
+
+    </div>
+  );
+}
+
+export default App;
